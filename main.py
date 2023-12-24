bb6212cf7aef35942443e971babd91619e384a5d
Benerman <benjaminwlarson@gmail.com>
2023-12-24 11:14:34 -0500

    working on flask and html

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
 import os
+import json
 from flask import Flask, render_template, Response
 import datetime
 
 
-data=[]
+data={}
 now=datetime.datetime.now()
-timeString=now.strftime("%Y-%m-%d %H:%M")
-templateData={
+time_string=now.strftime("%Y-%m-%d %H:%M")
+template_data={
 	'title':'Raspberry Pi 3B+ Web Controller',
-	'time':timeString,
+	'time':time_string,
 	'data':data,
 }
 
 app=Flask(__name__)
-def getData():
+def get_data():
     # Do something here
-	return
+	conns = load_json()
+	data = {**conns}
+	return data
+
+def load_json(file_path="previous_connections.json"):
+	with open(file_path) as fp:
+			return json.load(fp)
+
+def update_action(action_type, data):
+	if action_type:
+		conns = load_json
+		print(f'{action_type =} - {data =}')
+		if action_type == "connect":
+			pass
+		elif action_type == "remove":
+			pass
+		elif action_type == "priority":
+			pass
+		else:
+			print(f"No action performed due to invalide 'action_type' - {action_type}")
+	
+
 
 @app.route('/')
 def index():
 	now=datetime.datetime.now()
-	timeString=now.strftime("%Y-%m-%d %H:%M")
-	data=getData()
-	templateData={
-		'title':'Raspberry Pi 3B+ Web Controller',
-		'time':timeString,
+	time_string=now.strftime("%Y-%m-%d %H:%M")
+	data=get_data()
+	template_data={
+		'title':'GenesisPiZ Web Interface',
+		'time':time_string,
 		'data':data,
+		'addresses': data['previous_addresses']	
 	}
-	return render_template('rpi3b_webcontroller.html',**templateData)           
+	# return render_template('rpi3b_webcontroller.html',**template_data)           
+	return render_template('index.html',**template_data)           
 
-@app.route('/<actionid>') 
-def handleRequest(actionid):
-	print("Button pressed : {}".format(actionid))
+@app.route('/<action_type>?<action_data>') 
+def handle_request(action_type, action_data):
+	print(f"Button pressed: action_type: {action_type} - action_data: {action_data}")
 	return "OK 200"   
 	                      	
 if __name__=='__main__':
